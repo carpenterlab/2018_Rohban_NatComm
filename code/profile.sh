838301120c8e39f43332796ab3ce8f852e845112
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2018-01-30 15:49:26 -0500

    generalized the processed plate file

--- code/profile.sh
+++ code/profile.sh
@@ -1,1 +1,1 @@
-parallel -j 1 './profile.R --name=2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad --batch=2016_04_01_a549_48hr_batch1 --plate={1} --dim=3000 --rdensity=0.1 --core=2' :::: ../input/processed_plates.txt
+parallel -j 1 './profile.R --name=2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad --batch=2016_04_01_a549_48hr_batch1 --plate={1} --dim=3000 --rdensity=0.1 --core=2' :::: ../input/processed_plates_repurposing.txt
