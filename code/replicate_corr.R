b61ee712ab4f8850cfe0e2ef1aeeb290a9869c0c
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2018-01-31 13:59:24 -0500

    added dose + removed controls from the consideration

--- code/replicate_corr.R
+++ code/replicate_corr.R
@@ -18,11 +18,21 @@ get.profiles <- function(profile.type) {
     x
   }
   
+  if (!"Metadata_mmoles_per_liter" %in% colnames(profiles.nrm)) {
+    profiles.nrm <- profiles.nrm %>%
+      mutate(Metadata_mmoles_per_liter = 10)
+  }
+  
+  profiles.nrm <- profiles.nrm %>%
+    filter(!is.na(Metadata_broad_sample) & Metadata_broad_sample != "DMSO") %>%
+    mutate(Metadata_Treatment = paste0(Metadata_broad_sample, "@", Metadata_mmoles_per_liter))
+  
   profiles.nrm
 }
 
 Pf.mean <- get.profiles("mean")
 Pf.cov <- get.profiles("cov")
+
 
 feat.mean <- Pf.mean %>% colnames()
 feat.mean <- feat.mean[which(!str_detect(feat.mean, "Metadata_"))]
@@ -30,15 +40,15 @@ feat.mean <- feat.mean[which(!str_detect(feat.mean, "Metadata_"))]
 feat.cov <- Pf.cov %>% colnames()
 feat.cov <- feat.cov[which(!str_detect(feat.cov, "Metadata_"))]
 
-thr.mean <- non.rep.cor(list(data = Pf.mean), grp.var = "Metadata_broad_sample", feat.mean, quant = 0.95)
-thr.cov <- non.rep.cor(list(data = Pf.cov), grp.var = "Metadata_broad_sample", feat.cov, quant = 0.95)
+thr.mean <- non.rep.cor(list(data = Pf.mean), grp.var = "Metadata_Treatment", feat.mean, quant = 0.95)
+thr.cov <- non.rep.cor(list(data = Pf.cov), grp.var = "Metadata_Treatment", feat.cov, quant = 0.95)
 
-rep.mean <- rep.cor(list(data = Pf.mean), grp.var = "Metadata_broad_sample", feat.mean)
-rep.cov <- rep.cor(list(data = Pf.cov), grp.var = "Metadata_broad_sample", feat.cov)
+rep.mean <- rep.cor(list(data = Pf.mean), grp.var = "Metadata_Treatment", feat.mean)
+rep.cov <- rep.cor(list(data = Pf.cov), grp.var = "Metadata_Treatment", feat.cov)
 
 rep.mean %>%
   ungroup() %>%
-  filter(!is.na(Metadata_broad_sample)) %>%
+  filter(!is.na(Metadata_Treatment)) %>%
   mutate(tot = n()) %>%
   filter(cr > thr.mean) %>%
   mutate(frac.strong = n()/tot) %>%
@@ -48,7 +58,7 @@ rep.mean %>%
 
 rep.cov %>%
   ungroup() %>%
-  filter(!is.na(Metadata_broad_sample)) %>%
+  filter(!is.na(Metadata_Treatment)) %>%
   mutate(tot = n()) %>%
   filter(cr > thr.cov) %>%
   mutate(frac.strong = n()/tot) %>%
