3bef75e325770b6228812ecac9d479bbfe1dcdb2
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2018-02-02 10:35:32 -0500

    added possibility of testing a range of k's

--- code/compare_mean_cov.R
+++ code/compare_mean_cov.R
@@ -14,8 +14,8 @@ cr.mean <- cr.melt.mean %>%
   summarise(value = max(value)) %>%
   reshape2::acast("Var1 ~ Var2") 
 
-k.snf <- 7  # neighborhood size in SNF
-k <- 3      # k top hits are used for classification
+k.snf <- 7     # neighborhood size in SNF
+k <- 1:10      # k top hits are used for classification
 
 cr.cov <- cr.melt.cov %>%
   select(Var1, Var2, value) %>%
@@ -36,64 +36,95 @@ metadata <- cr.melt.mean %>%
   mutate(Metadata_broad_sample = Var1, Metadata_moa = Metadata_moa.x) %>%
   select(-Var1, -Metadata_moa.x)
 
+cmpd_classification <- Vectorize(cmpd_classification, "k0")
+cmpd_knn_classification <- Vectorize(cmpd_knn_classification, "k0")
+
 if (enrichment.based.classification) {
   d.mean <- cmpd_classification(sm = cr.mean, metadata = metadata, k0 = k)
   d.mix <- cmpd_classification(sm = cr.mix, metadata = metadata, k0 = k)
   
-  d.mean.sel <- d.mean %>%
-    filter(pass)
-  
-  d.mix.sel <- d.mix %>%
-    filter(pass)
-  
-  d.mean.sel %>% NROW() %>% print
-  d.mix.sel %>% NROW() %>% print
-  
-  diff.cmpds <- setdiff(d.mix.sel$Var1, d.mean.sel$Var1)
-  d.mix %>%
-    filter(Var1 %in% diff.cmpds) %>%
-    select(-Metadata_moa.x, -pass) %>%
-    left_join(d.mean, by = "Var1") %>%
-    select(-pass) %>%
-    rename(p.val.mix = p.val.x, p.val.mean = p.val.y, Compound = Var1) %>%
-    arrange(p.val.mix) %>%
-    knitr::kable() %>%
-    print()
-  
-  diff.cmpds <- setdiff(d.mean.sel$Var1, d.mix.sel$Var1)
-  d.mean %>%
-    filter(Var1 %in% diff.cmpds) %>%
-    select(-Metadata_moa.x, -pass) %>%
-    left_join(d.mix, by = "Var1") %>%
-    select(-pass) %>%
-    rename(p.val.mix = p.val.y, p.val.mean = p.val.x, Compound = Var1) %>%
-    arrange(p.val.mean) %>%
-    knitr::kable() %>%
-    print()
+  if (length(k) == 1) {
+    d.mean.sel <- d.mean %>%
+      filter(pass)
+    
+    d.mix.sel <- d.mix %>%
+      filter(pass)
+    
+    d.mean.sel %>% NROW() %>% print
+    d.mix.sel %>% NROW() %>% print
+    
+    diff.cmpds <- setdiff(d.mix.sel$Var1, d.mean.sel$Var1)
+    d.mix %>%
+      filter(Var1 %in% diff.cmpds) %>%
+      select(-Metadata_moa.x, -pass) %>%
+      left_join(d.mean, by = "Var1") %>%
+      select(-pass) %>%
+      rename(p.val.mix = p.val.x, p.val.mean = p.val.y, Compound = Var1) %>%
+      arrange(p.val.mix) %>%
+      knitr::kable() %>%
+      print()
+    
+    diff.cmpds <- setdiff(d.mean.sel$Var1, d.mix.sel$Var1)
+    d.mean %>%
+      filter(Var1 %in% diff.cmpds) %>%
+      select(-Metadata_moa.x, -pass) %>%
+      left_join(d.mix, by = "Var1") %>%
+      select(-pass) %>%
+      rename(p.val.mix = p.val.y, p.val.mean = p.val.x, Compound = Var1) %>%
+      arrange(p.val.mean) %>%
+      knitr::kable() %>%
+      print()
+  } else {
+    l.mean <- lapply(d.mean, function(x) sum(x$pass))
+    l.mix <- lapply(d.mix, function(x) sum(x$pass))
+    
+    D <- data.frame(method = "mean", k = k, tp = (unlist(l.mean)))
+    D <- rbind(D, 
+               data.frame(method = "mean+cov.", k = k, tp = (unlist(l.mix))))
+    g <- ggplot(D, aes(x = k, y = tp, color = method)) + 
+      geom_point() + 
+      geom_line() + 
+      ylab("No. of compounds with a same MOA compound in its k-NNs")
+    print(g)
+  }
 } else {
   d.mean <- cmpd_knn_classification(cr.mean, metadata, k) 
   d.mix <- cmpd_knn_classification(cr.mix, metadata, k) 
   
-  d.mean %>% NROW %>% print
-  d.mix %>% NROW %>% print
-  d.diff <- d.mean %>%
-    full_join(d.mix, by = "Var1") %>%
-    mutate(Metadata_moa = ifelse(is.na(Metadata_moa.x.x), Metadata_moa.x.y, Metadata_moa.x.x)) %>%
-    select(-Metadata_moa.x.x, -Metadata_moa.x.y) %>%
-    mutate(pass.mean = ifelse(is.na(pass.x), "No", "Yes"),
-           pass.mix = ifelse(is.na(pass.y), "No", "Yes")) %>%
-    select(-pass.x, -pass.y) %>% 
-    arrange(pass.mix) 
+  if (length(k) == 1) {
+    d.mean %>% NROW %>% print
+    d.mix %>% NROW %>% print
+    d.diff <- d.mean %>%
+      full_join(d.mix, by = "Var1") %>%
+      mutate(Metadata_moa = ifelse(is.na(Metadata_moa.x.x), Metadata_moa.x.y, Metadata_moa.x.x)) %>%
+      select(-Metadata_moa.x.x, -Metadata_moa.x.y) %>%
+      mutate(pass.mean = ifelse(is.na(pass.x), "No", "Yes"),
+             pass.mix = ifelse(is.na(pass.y), "No", "Yes")) %>%
+      select(-pass.x, -pass.y) %>% 
+      arrange(pass.mix) 
     
-  d.diff %>%  
-    filter(pass.mix == "Yes" & pass.mean == "No") %>%
-    arrange(Metadata_moa) %>%
-    htmlTable::htmlTable() %>%
-    print
-  
-  d.diff %>%  
-    filter(pass.mix == "No" & pass.mean == "Yes") %>%
-    arrange(Metadata_moa) %>%
-    htmlTable::htmlTable() %>%
-    print
+    d.diff %>%  
+      filter(pass.mix == "Yes" & pass.mean == "No") %>%
+      arrange(Metadata_moa) %>%
+      htmlTable::htmlTable() %>%
+      print
+    
+    d.diff %>%  
+      filter(pass.mix == "No" & pass.mean == "Yes") %>%
+      arrange(Metadata_moa) %>%
+      htmlTable::htmlTable() %>%
+      print
+  } else {
+    l.mean <- lapply(d.mean, function(x) sum(x$pass))
+    l.mix <- lapply(d.mix, function(x) sum(x$pass))
+    
+    D <- data.frame(method = "mean", k = k, tp = (unlist(l.mean)))
+    D <- rbind(D, 
+               data.frame(method = "mean+cov.", k = k, tp = (unlist(l.mix))))
+    g <- ggplot(D, aes(x = k, y = tp, color = method)) + 
+      geom_point() + 
+      geom_line() + 
+      ylab("No. of compounds with a same MOA compound in its k-NNs")
+    print(g)    
+  }
 }
