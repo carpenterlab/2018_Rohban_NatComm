61e20bb5ff7acbc7f92cda3a4722d2df3ffb7a94
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2018-02-05 12:38:30 -0500

    replace interaction data with the normal compound evaluation, where Pathway is the equivalent to MOA.

--- code/compare_mean_cov_genetic.R
+++ code/compare_mean_cov_genetic.R
@@ -1,27 +1,60 @@
 rm(list = ls())
 
 library(dplyr)
+library(ggplot2)
+
 source("moa_evaluations.R")
+
+enrichment.based.classification <- FALSE
+k.snf <- 7     # neighborhood size in SNF
+k <- 1:10      # k top hits are used for classification
 
 cr.melt.mean <- readRDS("cr_mean.rds")
 cr.melt.cov <- readRDS("cr_cov.rds")
 
 cr.mean <- cr.melt.mean %>%
   select(Var1, Var2, value) %>%
+  group_by(Var1, Var2) %>%
+  summarise(value = max(value)) %>%
   reshape2::acast("Var1 ~ Var2") 
-
-k.snf <- round(NROW(cr.mean)^0.5 / 4)
 
 cr.cov <- cr.melt.cov %>%
   select(Var1, Var2, value) %>%
-  reshape2::acast("Var1 ~ Var2") 
+  group_by(Var1, Var2) %>%
+  summarise(value = max(value)) %>%
+  reshape2::acast("Var1 ~ Var2")
 
-af.1 <- SNFtool::affinityMatrix(Diff = 1 - cr.mean, K = k.snf, sigma = 0.5)
-af.2 <- SNFtool::affinityMatrix(Diff = 1 - cr.mean, K = k.snf, sigma = 0.5)
-af.snf <- SNFtool::SNF(list(af.1, af.2), K = k.snf, t = 10)
-rownames(af.snf) <- rownames(af.1)
-colnames(af.snf) <- colnames(af.1)
-cr.mean.snf <- af.snf
+metad <- readr::read_csv("../input/metadata_TA_2.csv")
+gene.s <- function(x) {
+  str_split(x, "_")[[1]][1]
+}
+allele.s <- function(x) {
+  str_split(x, "_")[[1]][2]
+}
+gene.s <- Vectorize(gene.s)
+allele.s <- Vectorize(allele.s)
+
+subs <- metad %>%
+  mutate(gene = gene.s(Metadata_Treatment),
+         allele = allele.s(Metadata_Treatment))  %>%
+  filter(str_detect(allele, "WT")) %>%
+  group_by(gene) %>%
+  slice(1) %>%
+  ungroup() %>%
+  select(Metadata_broad_sample) %>%
+  as.matrix() %>%
+  as.vector()
+
+cr.mean <- cr.mean[subs, subs]
+d <- apply(cr.mean, 1, function(x) !(sum(is.na(x)) >= (NROW(cr.mean) -1 )))
+cr.mean <- cr.mean[d, d]
+
+d <- apply(cr.cov, 1, function(x) !(sum(is.na(x)) >= (NROW(cr.cov) -1 )))
+cr.cov <- cr.cov[d, d]
+
+cm.rn <- setdiff(intersect(rownames(cr.mean), rownames(cr.cov)), NA)
+cr.mean <- cr.mean[cm.rn, cm.rn]
+cr.cov <- cr.cov[cm.rn, cm.rn]
 
 af.1 <- SNFtool::affinityMatrix(Diff = 1 - cr.mean, K = k.snf, sigma = 0.5)
 af.2 <- SNFtool::affinityMatrix(Diff = 1 - cr.cov, K = k.snf, sigma = 0.5)
@@ -30,17 +63,113 @@ rownames(af.snf) <- rownames(af.1)
 colnames(af.snf) <- colnames(af.1)
 cr.mix <- af.snf
 
-ppi <- readRDS("../input/pr.pr.binary.rds")
-ppi.extra <- readRDS("../input/pr.pr.interaction.rds")
+metadata <- cr.melt.mean %>%
+  select(Var1, Metadata_moa.x) %>%
+  unique() %>%
+  mutate(Metadata_broad_sample = Var1, Metadata_moa = Metadata_moa.x) %>%
+  select(-Var1, -Metadata_moa.x)
 
-metadata.2 <- readr::read_csv("../input/metadata_TA_2.csv")
-metadata.1 <- readr::read_csv("../input/metadata_TA.csv")
+cmpd_classification <- Vectorize(cmpd_classification, "k0")
+cmpd_knn_classification <- Vectorize(cmpd_knn_classification, "k0")
 
-metadata.2 <- metadata.2 %>%
-  semi_join(metadata.1, by = "Metadata_broad_sample")
-
-protein.interaction.comparison(sm = cr.mean, metadata = metadata.2, interaction.data = ppi, extra.interaction.data = ppi.extra, quant = 0.95, suffix = "mean")
-protein.interaction.comparison(sm = cr.mix, metadata = metadata.2, interaction.data = ppi, extra.interaction.data = ppi.extra, quant = 0.95, suffix = "mix")
-
-#print(d.mean)
-#print(d.mix)
+if (enrichment.based.classification) {
+  d.mean <- cmpd_classification(sm = cr.mean, metadata = metadata, k0 = k)
+  d.mix <- cmpd_classification(sm = cr.mix, metadata = metadata, k0 = k)
+  
+  if (length(k) == 1) {
+    d.mean <- (d.mean %>% t) %>% apply(., 2, function(x) {y <- data.frame(as.vector(x)); names(y) <- names(x); y}) 
+    d.mix <- (d.mix %>% t) %>% apply(., 2, function(x) {y <- data.frame(as.vector(x)); names(y) <- names(x); y}) 
+    
+    d.mean <- d.mean %>% as.data.frame()
+    d.mix <- d.mix %>% as.data.frame()
+    
+    d.mean.sel <- d.mean %>%
+      filter(pass)
+    
+    d.mix.sel <- d.mix %>%
+      filter(pass)
+    
+    d.mean.sel %>% NROW() %>% print
+    d.mix.sel %>% NROW() %>% print
+    
+    diff.cmpds <- setdiff(d.mix.sel$Var1, d.mean.sel$Var1)
+    d.mix %>%
+      filter(Var1 %in% diff.cmpds) %>%
+      select(-Metadata_moa.x, -pass) %>%
+      left_join(d.mean, by = "Var1") %>%
+      select(-pass) %>%
+      rename(p.val.mix = p.val.x, p.val.mean = p.val.y, Compound = Var1) %>%
+      arrange(p.val.mix) %>%
+      knitr::kable() %>%
+      print()
+    
+    diff.cmpds <- setdiff(d.mean.sel$Var1, d.mix.sel$Var1)
+    d.mean %>%
+      filter(Var1 %in% diff.cmpds) %>%
+      select(-Metadata_moa.x, -pass) %>%
+      left_join(d.mix, by = "Var1") %>%
+      select(-pass) %>%
+      rename(p.val.mix = p.val.y, p.val.mean = p.val.x, Compound = Var1) %>%
+      arrange(p.val.mean) %>%
+      knitr::kable() %>%
+      print()
+  } else {
+    l.mean <- lapply(d.mean["pass", ], function(x) sum(x)) 
+    l.mix <- lapply(d.mix["pass", ], function(x) sum(x))
+    
+    D <- data.frame(method = "mean", k = k, tp = (unlist(l.mean)))
+    D <- rbind(D, 
+               data.frame(method = "mean+cov.", k = k, tp = (unlist(l.mix))))
+    g <- ggplot(D, aes(x = k, y = tp, color = method)) + 
+      geom_point() + 
+      geom_line() + 
+      scale_y_continuous(limits = c(0, NA)) +
+      scale_x_continuous(breaks = k, minor_breaks = k) +
+      ylab("No. of compounds with a \n same MOA compound in their k-NNs")
+    print(g)    
+    ggsave("classification_comparison.png", g, width = 7, height = 5)
+  }
+} else {
+  d.mean <- cmpd_knn_classification(cr.mean, metadata, k) 
+  d.mix <- cmpd_knn_classification(cr.mix, metadata, k) 
+  
+  if (length(k) == 1) {
+    d.mean %>% NROW %>% print
+    d.mix %>% NROW %>% print
+    d.diff <- d.mean %>%
+      full_join(d.mix, by = "Var1") %>%
+      mutate(Metadata_moa = ifelse(is.na(Metadata_moa.x.x), Metadata_moa.x.y, Metadata_moa.x.x)) %>%
+      select(-Metadata_moa.x.x, -Metadata_moa.x.y) %>%
+      mutate(pass.mean = ifelse(is.na(pass.x), "No", "Yes"),
+             pass.mix = ifelse(is.na(pass.y), "No", "Yes")) %>%
+      select(-pass.x, -pass.y) %>% 
+      arrange(pass.mix) 
+    
+    d.diff %>%  
+      filter(pass.mix == "Yes" & pass.mean == "No") %>%
+      arrange(Metadata_moa) %>%
+      htmlTable::htmlTable() %>%
+      print
+    
+    d.diff %>%  
+      filter(pass.mix == "No" & pass.mean == "Yes") %>%
+      arrange(Metadata_moa) %>%
+      htmlTable::htmlTable() %>%
+      print
+  } else {
+    l.mean <- lapply(d.mean["pass", ], function(x) sum(x)) 
+    l.mix <- lapply(d.mix["pass", ], function(x) sum(x))
+    
+    D <- data.frame(method = "mean", k = k, tp = (unlist(l.mean)))
+    D <- rbind(D, 
+               data.frame(method = "mean+cov.", k = k, tp = (unlist(l.mix))))
+    g <- ggplot(D, aes(x = k, y = tp, color = method)) + 
+      geom_point() + 
+      geom_line() + 
+      scale_y_continuous(limits = c(0, NA)) +
+      scale_x_continuous(breaks = k, minor_breaks = k) +
+      ylab("No. of genes with a \n same Pathway gene in their k-NNs")
+    print(g) 
+    ggsave("classification_comparison.png", g, width = 7, height = 5)
+  }
+}
