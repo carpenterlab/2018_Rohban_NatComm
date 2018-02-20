8b591b1036ab1bb94ec3c45cb883b2669e6d37e8
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2018-02-20 15:24:55 -0500

    repurposing

--- code/profile_trad.sh
+++ code/profile_trad.sh
@@ -4,5 +4,5 @@
 # CDRP		2015_Bray_GigaScience						CDRP				processed_plates_CDRP_bio.txt
 # Repurposing	2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad	2016_04_01_a549_48hr_batch1	processed_plates_repurposing.txt
 
-parallel -j 1 './profile_trad.R --name=2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad --batch=2016_04_01_a549_48hr_batch1 --plate={1} --feats="../input/feature_list.txt" --operation="median+mad" --col="Metadata_broad_sample" --value="DMSO" --cores=2' :::: '../input/processed_plates_repurposing copy.txt'
+parallel -j 1 './profile_trad.R --name=2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad --batch=2016_04_01_a549_48hr_batch1 --plate={1} --feats="../input/feature_list.txt" --operation="median+mad" --col="Metadata_broad_sample" --value="DMSO" --cores=2' :::: '../input/processed_plates_repurposing.txt'
 
