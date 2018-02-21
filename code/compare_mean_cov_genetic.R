a84e2fdcf65d6d04b40e2a0d3c9342373b00774b
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2018-02-21 18:29:40 -0500

    median as the baseline

--- code/compare_mean_cov_genetic.R
+++ code/compare_mean_cov_genetic.R
@@ -9,7 +9,7 @@ enrichment.based.classification <- FALSE
 k.snf <- 7     # neighborhood size in SNF
 k <- 1:10      # k top hits are used for classification
 
-cr.melt.mean <- readRDS("cr_mean.rds")
+cr.melt.mean <- readRDS("cr_median.rds")
 cr.melt.cov <- readRDS("cr_cov.rds")
 cr.melt.median.mad <- readRDS("cr_median+mad.rds")
 
@@ -170,9 +170,9 @@ if (enrichment.based.classification) {
     l.median.mad <- lapply(d.median.mad["pass", ], function(x) sum(x)) 
     l.mix <- lapply(d.mix["pass", ], function(x) sum(x))
     
-    D <- data.frame(method = "mean", k = k, tp = (unlist(l.mean)))
+    D <- data.frame(method = "median", k = k, tp = (unlist(l.mean)))
     D <- rbind(D, 
-               data.frame(method = "mean+cov.", k = k, tp = (unlist(l.mix))))
+               data.frame(method = "median+cov.", k = k, tp = (unlist(l.mix))))
     D <- rbind(D, 
                data.frame(method = "median+mad", k = k, tp = (unlist(l.median.mad))))
     D <- D %>% mutate(method = factor(method, levels = sort(unique(as.character(D$method)))))
@@ -199,9 +199,9 @@ median.mad.res <- median.mad.res["estimate",] %>% unlist %>% unname()
 mix.res <- mix.res["estimate",] %>% unlist %>% unname()
 
 
-D1 <- data.frame(top.prec = top.prec * 100, odds.ratio = mean.res, method = "mean")
+D1 <- data.frame(top.prec = top.prec * 100, odds.ratio = mean.res, method = "median")
 D2 <- data.frame(top.prec = top.prec * 100, odds.ratio = median.mad.res, method = "median+mad")
-D3 <- data.frame(top.prec = top.prec * 100, odds.ratio = mix.res, method = "mean+cov.")
+D3 <- data.frame(top.prec = top.prec * 100, odds.ratio = mix.res, method = "median+cov.")
 
 D <- rbind(D1, D2)
 D <- rbind(D, D3)
