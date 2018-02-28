7c68445efdae6f503e271358bbe5f52e41ba7675
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2018-02-27 22:48:18 -0500

    plot corr hits

---
+++ code/plot_corr_hist.R
@@ -0,0 +1,8 @@
+x1 <- (cr.mean)
+x2 <- (cr.cov)
+
+x <- setdiff(x1, 1)
+y <- setdiff(x2, 1)
+
+D <- rbind(data.frame(method = "cov", x = y), data.frame(method = "med.", x = x))
+ggplot(D, aes(x = x, group = method, fill = method)) + geom_histogram(aes(y = ..density..), position="identity", alpha = 0.8, bins = 100)
